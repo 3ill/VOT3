ecfc7b1a9e3aecdc98a83cde563f20b4cf85f48b
3illBaby <thrillanon@outlook.com>
2023-08-03 21:06:26 +0100

    added configs and packages

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
+const { GOERLI_URL, PRIVATE_KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: '0.8.18',
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 5,
+      blockConfirmations: 1,
+    },
+  },
+};
